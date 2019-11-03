a91737ca075781dd05ee3132622522d30de87e24
Charlie Chao <charliechao@outlook.com>
2019-11-03 12:42:07 -0800

    refactor(modal): clean up

--- src/modal.js
+++ src/modal.js
@@ -1,46 +1,43 @@
-import BrowserWindow from 'sketch-module-web-view'
-import { getWebview } from 'sketch-module-web-view/remote'
-import UI from 'sketch/ui'
+import BrowserWindow from "sketch-module-web-view"
+import { getWebview } from "sketch-module-web-view/remote"
+import UI from "sketch/ui"
 
-const webviewIdentifier = 'sketch-push.webview'
+const webviewIdentifier = "sketch-push.webview"
 
-export default function () {
+export default function() {
+  // https://github.com/skpm/sketch-module-web-view/blob/master/docs/browser-window.md
   const options = {
     identifier: webviewIdentifier,
     width: 240,
     height: 180,
     show: false,
-    titleBarStyle: 'hidden',
+    titleBarStyle: "hidden",
     remembersWindowFrame: true
   }
 
   const browserWindow = new BrowserWindow(options)
 
-  // only show the window when the page has loaded to avoid a white flash
-  browserWindow.once('ready-to-show', () => {
+  browserWindow.once("ready-to-show", () => {
     browserWindow.show()
   })
 
   const webContents = browserWindow.webContents
 
-  // print a message when the page loads
-  webContents.on('did-finish-load', () => {
-    UI.message('UI loaded!')
+  webContents.on("did-finish-load", () => {
+    UI.message("UI loaded!")
   })
 
-  // add a handler for a call from web content's javascript
-  webContents.on('nativeLog', s => {
+  // Handle calls from view
+  webContents.on("nativeLog", s => {
     UI.message(s)
     webContents
       .executeJavaScript(`setRandomNumber(${Math.random()})`)
       .catch(console.error)
   })
 
-  browserWindow.loadURL(require('../resources/webview.html'))
+  browserWindow.loadURL(require("../resources/webview.html"))
 }
 
-// When the plugin is shutdown by Sketch (for example when the user disable the plugin)
-// we need to close the webview if it's open
 export function onShutdown() {
   const existingWebview = getWebview(webviewIdentifier)
   if (existingWebview) {
