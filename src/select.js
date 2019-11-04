b4f7c7a66476a5b33555d4adcb34e04efb2cf215
Charlie Chao <charliechao@outlook.com>
2019-11-03 16:57:58 -0800

    feat: add select layers above/below/left/right

--- src/select.js
+++ src/select.js
@@ -3,19 +3,31 @@
  */
 
 import sketch from "sketch"
+import { getSingleSelection, getArtboard, getLayers } from "./utils"
+
+function select(direction) {
+  const selection = getSingleSelection()
+  if (!selection) return
+
+  // Select layers
+  const artboard = getArtboard(selection)
+  const { layers } = getLayers(direction, selection, artboard)
+  const doc = sketch.Document.getSelectedDocument()
+  doc.selectedLayers = layers
+}
 
 export function selectAbove() {
-  // TODO: Select above
+  select("above")
 }
 
 export function selectBelow() {
-  // TODO: Select below
+  select("below")
 }
 
 export function selectLeft() {
-  // TODO: Select left
+  select("left")
 }
 
 export function selectRight() {
-  // TODO: Select right
+  select("right")
 }
