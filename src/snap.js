599f5d717cd86efd6abf680ea359bb103abba898
Charlie Chao <charliechao@outlook.com>
2019-11-03 16:57:49 -0800

    feat: add snap to top/bottom/left/right edge

--- src/snap.js
+++ src/snap.js
@@ -3,80 +3,65 @@
  */
 
 import sketch from "sketch"
-import { getArtboard, getLayersBelow } from "./utils"
+import { getSingleSelection, getArtboard, getLayers } from "./utils"
 
-export function snapTop() {
-  // TODO: Snap to top
-}
+function snapTo(edge) {
+  const doc = sketch.Document.getSelectedDocument()
+  const selection = getSingleSelection()
+  if (!selection) return
 
-export function snapBottom() {
-  var doc = sketch.Document.getSelectedDocument()
-  var selection = doc.selectedLayers
+  // Get layers to move and the position closest to target edge
+  const context =
+    selection.type === "Artboard" ? doc.selectedPage : getArtboard(selection)
+  const { layers, position } = getLayers(
+    edge === "top" ? "above" : edge === "bottom" ? "below" : edge,
+    selection,
+    context
+  )
 
-  if (selection.length === 0) {
-    sketch.UI.message("Please select a group or layer.")
-    return
+  // Determine change in position
+  let diffX = 0
+  let diffY = 0
+  const frame = selection.frame.changeBasis({
+    from: selection.parent,
+    to: context
+  })
+  if (edge === "top") {
+    diffY = frame.y - position
+  } else if (edge === "bottom") {
+    diffY = frame.y + frame.height - position
+  } else if (edge === "left") {
+    diffX = frame.x - position
+  } else {
+    diffX = frame.x + frame.width - position
   }
 
-  let items = {}
-  let count = 0
-  for (const layer of selection.layers) {
-    const artboard = getArtboard(layer)
-    if (!artboard || artboard === layer) continue
-
-    const id = artboard.id
-    if (!(id in items)) {
-      // Create artboard item
-      items[id] = {
-        artboard,
-        target: null,
-        top: Number.MAX_SAFE_INTEGER,
-        bottom: 0
-      }
-    }
-
-    const frame = layer.frame.changeBasis({
-      from: layer.parent,
-      to: artboard
-    })
-    if (frame.y < items[id].top) {
-      // Set top-most layer as target
-      items[id].target = layer
-      items[id].top = frame.y
-      items[id].bottom = frame.y + frame.height
-    }
-    count++
-  }
-
-  if (!count) {
-    sketch.UI.message("Please select groups/layers, not artboards.")
-    return
-  }
-
-  for (const key in items) {
-    if (!items.hasOwnProperty(key)) continue
-    const item = items[key]
-    const artboard = item.artboard
-
-    // Find all layers below target
-    const { layers, top } = getLayersBelow(artboard, item.target, item.top)
-
-    // Push layers to bottom of target
-    const diff = item.bottom - top
-    for (const layer of layers) {
-      layer.frame.y += diff
-      const parent = layer.parent
-      if (parent.type === "Group") {
-        parent.adjustToFit()
-      }
+  // Move layers
+  for (const layer of layers) {
+    layer.frame.x += diffX
+    layer.frame.y += diffY
+    const parent = layer.parent
+    if (parent.type === "Group") {
+      parent.adjustToFit()
     }
   }
+
+  // Select moved layers
+  doc.selectedLayers = layers
 }
 
-export function snapLeft() {
-  // TODO: Snap to left
+export function snapToTop() {
+  snapTo("top")
 }
 
-export function snapRight() {
-  // TODO: Snap to right
+export function snapToBottom() {
+  snapTo("bottom")
+}
+
+export function snapToLeft() {
+  snapTo("left")
+}
+
+export function snapToRight() {
+  snapTo("right")
 }
--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,23 @@
  * Utilities
  */
 
+import sketch from "sketch"
+
+function getSingleSelection() {
+  const doc = sketch.Document.getSelectedDocument()
+  const selections = doc.selectedLayers
+
+  if (selections.isEmpty) {
+    sketch.UI.message("Please select a layer or artboard.")
+    return null
+  } else if (selections.length !== 1) {
+    sketch.UI.message("Please select only one layer or artboard.")
+    return null
+  }
+
+  return selections.layers[0]
+}
+
 function getArtboard(layer) {
   if (layer.type === "Artboard") {
     return layer
@@ -11,33 +28,97 @@ function getArtboard(layer) {
   return null
 }
 
-function getLayersBelow(artboard, target, targetTop, layer, output) {
+function getParentIds(layer, parents) {
+  parents = parents || []
+  if (!layer.parent) return parents
+  parents.push(layer.parent.id)
+  return getParentIds(layer.parent, parents)
+}
+
+function getLayers(
+  direction,
+  target,
+  context,
+  targetFrame,
+  targetParentIds,
+  layer,
+  output
+) {
   output = output || {
     layers: [],
-    top: Number.MAX_SAFE_INTEGER
+    position:
+      direction === "above" || direction === "left"
+        ? -Number.MAX_SAFE_INTEGER
+        : Number.MAX_SAFE_INTEGER
   }
-  layer = layer || artboard
-  const frame = layer.frame.changeBasis({
-    from: layer.parent,
-    to: artboard
-  })
+  targetFrame =
+    targetFrame ||
+    target.frame.changeBasis({
+      from: target.parent,
+      to: context
+    })
+  targetParentIds = targetParentIds || getParentIds(target)
+  layer =
+    !layer && target.type === "Artboard"
+      ? /* If target is an artboard, start from page */ target.parent
+      : layer || context
+
   if (layer.id === target.id) return output
-  if (
-    layer.id !== target.parent.id &&
-    layer.type !== "Artboard" &&
-    frame.y >= targetTop
-  ) {
-    output.top = Math.min(output.top, frame.y)
-    output.layers.push(layer)
+
+  // Determine if layer is in region
+  const frame = layer.frame.changeBasis({
+    from:
+      layer.type === "Page"
+        ? /* Use page coordinate space */ layer
+        : layer.parent,
+    to: context
+  })
+  let position = 0
+  let isInRegion = false
+  if (direction === "above") {
+    // Layer is above target's bottom edge
+    position = frame.y + frame.height
+    isInRegion = position <= targetFrame.y + targetFrame.height
+  } else if (direction === "below") {
+    // Layer is below target's top edge
+    position = frame.y
+    isInRegion = position >= targetFrame.y
+  } else if (direction === "left") {
+    // Layer is left of target's right edge
+    position = frame.x + frame.width
+    isInRegion = position <= targetFrame.x + targetFrame.width
   } else {
+    // Layer is right of target's left edge
+    position = frame.x
+    isInRegion = position >= targetFrame.x
+  }
+
+  if (!targetParentIds.includes(layer.id) && isInRegion) {
+    // Add to list
+    output.layers.push(layer)
+    output.position =
+      direction === "above" || direction === "left"
+        ? Math.max(output.position, position)
+        : Math.min(output.position, position)
+  } else {
+    // Traverse child layers
     const children = layer.layers
     if (children && children.length) {
       for (const child of children) {
-        output = getLayersBelow(artboard, target, targetTop, child, output)
+        output = getLayers(
+          direction,
+          target,
+          context,
+          targetFrame,
+          targetParentIds,
+          child,
+          output
+        )
       }
     }
   }
+
   return output
 }
 
-export { getArtboard, getLayersBelow }
+export { getSingleSelection, getArtboard, getParentIds, getLayers }
